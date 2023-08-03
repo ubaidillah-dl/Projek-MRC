81f5f416bc4de25a356ff26b8c4ba62cf0e2b3ee
ubaidillah-dl <ubayd2947@gmail.com>
2023-08-03 08:02:23 +0700

    8:02

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 // koneksi database
-$conn = mysqli_connect("localhost", "root", "", "event_mrc");
+$conn = mysqli_connect("localhost", "event_mrc", "MRC@CY!WSVQuNuzlWkVzXP5g", "event_mrc");
 
 function query($query)
 {
