46eddef93c40c9d0429eaa85377181d159aec7b7
ubaidillah-dl <ubayd2947@gmail.com>
2023-08-06 14:53:20 +0700

    tesssss

--- daftar.php
+++ daftar.php
@@ -1,29 +1,30 @@
 <?php
-// konek database
-require "functions.php";
 
-// cek tombol kirim sudah ditekan atau belum
+require "fungsi.php";
+
+$error = "";
 if (isset($_POST["kirim"])) {
 
-    if (tambah($_POST) > 0) {
-        echo    "
-                <script>
-                    alert('Berhasil mendaftar di MRC 2023 !');
-                    document.location.href = 'https://chat.whatsapp.com/BcvSS6QzrZFHr2PKZRVj41'; 
-                </script>
-                ";
+    if (daftar($_POST) > 0) {
+        echo    '
+        <script>
+        alert("Berhasil mendaftar di MRC 2023 !");
+        document.location.href = "https://chat.whatsapp.com/BcvSS6QzrZFHr2PKZRVj41"; 
+        </script>
+        ';
     } else {
-        echo    "
-                <script>
-                    alert('Gagal mendaftar di MRC 2023 !');
-                    document.location.href = 'daftar.php'; 
-                </script>
-                ";
+        // $error = "Gagal mendaftar di MRC 2023 Pastikan ekstensi file jpg, jpeg, png atau pdf maksimal 1MB !";
+        $error = 'Gagal mendaftar di MRC 2023 </br>Pastikan ekstensi file jpg, jpeg, png atau pdf maksimal 1MB !';
+        // echo    '
+        //             <script>
+        //                 alert("Gagal mendaftar di MRC 2023 \n\nPastikan ekstensi file jpg, jpeg, png atau pdf maksimal 1MB !");
+        //                 document.location.href = "daftar.php"; 
+        //             </script>
+        //         ';
     }
 }
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -37,8 +38,7 @@ if (isset($_POST["kirim"])) {
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&family=Prompt:wght@100;300;400;700&display=swap"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&family=Prompt:wght@100;300;400;700&display=swap" rel="stylesheet" />
 
     <!-- CSS -->
     <link rel="stylesheet" href="css/daftar.css" />
@@ -62,62 +62,63 @@ if (isset($_POST["kirim"])) {
             <!-- Kontent start -->
             <div class="content">
                 <form action="" method="post" enctype="multipart/form-data">
-                    <div class="nama-tim input"><label for="nama-tim">Nama Tim</label><input required autofocus
-                            autocomplete="off" type="text" placeholder="Masukkan Nama Tim" id="nama-tim"
-                            name="nama_tim" /></div>
-                    <div class="nama-instansi input"><label for="nama-instansi">Nama Instansi</label><input
-                            autocomplete="off" required type="text" id="nama-instansi"
-                            placeholder="Masukkan Nama Instansi" name="nama_instansi" />
+                    <!-- Nama tim start -->
+                    <div class="nama-tim input"><label for="nama-tim">Nama Tim</label><input autofocus autocomplete="off" type="text" placeholder="Masukkan Nama Tim" id="nama-tim" name="nama_tim" /></div>
+                    <!-- Nama tim end -->
+
+                    <!-- Nama instansi start -->
+                    <div class="nama-instansi input"><label for="nama-instansi">Nama Instansi</label><input autocomplete="off" type="text" id="nama-instansi" placeholder="Masukkan Nama Instansi" name="nama_instansi" /></div>
+                    <!-- Nama instansi start -->
+
+                    <!-- Nama Pembina start -->
+                    <div class="nama-pembina input">
+                        <label for="nama-pembina">Nama Pembina <span style="color: var(--secondary); font-size: 0.8rem; font-style: italic">(tidak
+                                wajib)</span></label><input autocomplete="off" name="nama_pembina" type="text" id="nama-pembina" placeholder="Masukkan Nama Pembina" />
                     </div>
-                    <div class="nama-pembina input"><label for="nama-pembina">Nama Pembina <span
-                                style="color: var(--secondary); font-size:0.8rem; font-style: italic">(tidak
-                                wajib)</span></label><input autocomplete="off" name="nama_pembina" type="text"
-                            id="nama-pembina" placeholder="Masukkan Nama Pembina" />
-                    </div>
+                    <!-- Nama Pembina end -->
 
-
-
+                    <!-- Bukti pembayaran start -->
                     <div class="bukti-pembayaran input">
                         <label for="bukti-pembayaran">Bukti Pembayaran</label>
-                        <input required autocomplete="off" type="file" id="bukti-pembayaran" name="bukti_pembayaran" />
+                        <input type="file" id="bukti-pembayaran" name="bukti_pembayaran" />
                     </div>
+                    <!-- Bukti pembayaran end -->
 
+                    <!-- Nama ketua start -->
+                    <div class="nama-ketua input"><label for="nama-ketua">Nama Ketua</label><input autocomplete="off" name="nama_ketua" type="text" id="nama-ketua" placeholder="Masukkan Nama Ketua" /></div>
+                    <!-- Nama ketua start -->
 
+                    <!-- Email ketua start -->
+                    <div class="email-ketua input"><label for="email-ketua">Email Ketua</label><input autocomplete="off" name="email_ketua" type="email" id="email-ketua" placeholder="Masukkan Email Ketua" /></div>
+                    <!-- Email ketua end -->
 
-                    <div class="nama-ketua input"><label for="nama-ketua">Nama Ketua</label><input autocomplete="off"
-                            required name="nama_ketua" type="text" id="nama-ketua" placeholder="Masukkan Nama Ketua" />
-                    </div>
-                    <div class="email-ketua input"><label for="email-ketua">Email Ketua</label><input autocomplete="off"
-                            required name="email_ketua" type="email" id="email-ketua"
-                            placeholder="Masukkan Email Ketua" /></div>
-                    <div class="wa-ketua input"><label for="wa-ketua">Nomor Whatsapp Ketua</label><input
-                            autocomplete="off" required name="nomor_whatsapp_ketua" type="tel" id="wa-ketua"
-                            placeholder="Masukkan Nomor Whatsapp Ketua" /></div>
+                    <!-- Nomor whatsapp ketua start -->
+                    <div class="wa-ketua input"><label for="wa-ketua">Nomor Whatsapp Ketua</label><input autocomplete="off" name="nomor_whatsapp_ketua" type="tel" id="wa-ketua" placeholder="Masukkan Nomor Whatsapp Ketua" /></div>
+                    <!-- Nomor whatsapp ketua ened -->
 
+                    <!-- Ktm ketua start -->
+                    <div class="ktm-ketua input"><label for="ktm-ketua">KTM Ketua</label><input autocomplete="off" class="unggah" name="ktm_ketua" type="file" id="ktm-ketua" /></div>
+                    <!-- Ktm ketua start -->
 
-
-                    <div class="ktm-ketua input"><label for="ktm-ketua">KTM Ketua</label><input required
-                            autocomplete="off" class="unggah" name="ktm_ketua" type="file" id="ktm-ketua" /></div>
-
-
+                    <!-- Bukti up twibbon ketua start -->
                     <div class="bukti-uptwibbon-ketua input"><label for="bukti-uptwibbon-ketua">Bukti Up Twibbon
-                            Ketua</label><input required autocomplete="off" class="unggah" type="file"
-                            name="bukti_up_twibbon_ketua" id="bukti-uptwibbon-ketua" /></div>
+                            Ketua</label><input autocomplete="off" class="unggah" type="file" name="bukti_up_twibbon_ketua" id="bukti-uptwibbon-ketua" /></div>
+                    <!-- Bukti up twibbon ketua end -->
 
+                    <!-- Nama anggota start -->
+                    <div class="nama-anggota input"><label for="nama-anggota">Nama Anggota</label><input autocomplete="off" name="nama_anggota" type="text" id="nama-anggota" placeholder="Masukkan Nama Anggota" /></div>
+                    <!-- Nama anggota start -->
 
-                    <div class="nama-anggota input"><label for="nama-anggota">Nama Anggota</label><input
-                            autocomplete="off" required name="nama_anggota" type="text" id="nama-anggota"
-                            placeholder="Masukkan Nama Anggota" />
+                    <!-- Ktm anggota start -->
+                    <div class="ktm-anggota input"><label for="ktm-anggota">KTM Anggota</label><input autocomplete="off" class="unggah" name="ktm_anggota" type="file" id="ktm-anggota" /></div>
+                    <!-- Ktm anggota end -->
+
+                    <!-- Bukti up twibbon anggota start -->
+                    <div class="bukti-uptwibbon-anggota input">
+                        <label for="bukti-uptwibbon-anggota">Bukti Up Twibbon Anggota</label><input autocomplete="off" class="unggah" type="file" id="bukti-uptwibbon-anggota" name="bukti_up_twibbon_anggota" />
                     </div>
-
-
-                    <div class="ktm-anggota input"><label for="ktm-anggota">KTM Anggota</label><input required
-                            autocomplete="off" class="unggah" name="ktm_anggota" type="file" id="ktm-anggota" /></div>
-
-
-                    <div class="bukti-uptwibbon-anggota input"><label for="bukti-uptwibbon-anggota">Bukti Up Twibbon
-                            Anggota</label><input required autocomplete="off" class="unggah" type="file"
-                            id="bukti-uptwibbon-anggota" name="bukti_up_twibbon_anggota" /></div>
+                    <div><?php echo $error; ?></div>
+                    <!-- Bukti up twibbon anggota end -->
 
                     <button type="submit" name="kirim">Kirim</button>
                 </form>
